6cf02dce950eeedbebf5334e094787408343e992
felipe_paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-09-22 09:57:06 -0300

    add - ex 8

---
+++ ex_8.php
@@ -0,0 +1,27 @@
+<?php
+
+
+function ordenarNomes($nomesTexto){
+
+    $vetorNomes = explode(",", $nomesTexto);
+    $vetorNomes = array_map("trim", $vetorNomes);
+    sort($vetorNomes);
+
+    return $vetorNomes;
+}
+
+$nomesUsuario = "Felipe, Caio, Lucas, Arthur, João";
+
+echo "Lista original: " . $nomesUsuario . "<br>";
+$nomesOrdenados = ordenarNomes($nomesUsuario);
+echo "Lista ordenada: " . implode(", ", $nomesOrdenados) . "<br>";
+
+foreach ($nomesOrdenados as $nome) {
+    echo $nome . "<br>";
+}
+
+foreach ($nomesOrdenados as $nome){
+    echo "- $nome <br>";
+
+}
+?>
